b3de2bab8b82f7aa7bda55ad626eac4f03d10813
Drew Pomatti <ampomatti@gmail.com>
2022-05-02 21:52:18 -0700

    added Carousel component for images and babel

--- src/Details.js
+++ src/Details.js
@@ -1,22 +1,37 @@
 import { Component } from "react"
-//import { useParams } from "react-router-dom";
-
+import { useParams } from "react-router-dom";
+import Carousel from "./Carousel"
 
 class Details extends Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
 
-        this.state = { loading: true };
+    //     this.state = { loading: true };
+    // }
+
+    state = { loading: true }
+
+    async componentDidMount() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+        );
+        const json = await res.json();
+
+        this.setState(Object.assign({ loading: false }, json.pets[0]));
+        //same but w/ spread operator ꜜ
+        //this.setState({ loading: false, ...json.pets[0] });
     }
+
     render() {
         if (this.state.loading) {
             return <h2>loading ...</h2>
         }
 
-        const { animal, breed, city, state, description, name } = this.state;
+        const { animal, breed, city, state, description, name, images } = this.state;
 
         return (
             <div className="details">
+                <Carousel images={images} />
                 <div>
                     <h1>{name}</h1>
                     <h2>
@@ -30,6 +45,10 @@ class Details extends Component {
     }
 }
 
+const WrappedDetails = () => {
+    const params = useParams();
+    return <Details params={params} />;
+};
 //can't use hooks in class comps
 
-export default Details;
+export default WrappedDetails;
