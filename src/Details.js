5d44f168d31d82f7b4dd90b67cfdb77b86253aae
Drew Pomatti <ampomatti@gmail.com>
2022-05-04 14:33:49 -0700

    added ErrorBoundary

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,10 @@
 import { Component } from "react"
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel"
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
-    // constructor(props) {
+    // constructor(props) { 
     //     super(props);
 
     //     this.state = { loading: true };
@@ -27,6 +28,8 @@ class Details extends Component {
             return <h2>loading ...</h2>
         }
 
+        throw new Error("lmao you crashed.")
+
         const { animal, breed, city, state, description, name, images } = this.state;
 
         return (
@@ -47,7 +50,11 @@ class Details extends Component {
 
 const WrappedDetails = () => {
     const params = useParams();
-    return <Details params={params} />;
+    return (
+        <ErrorBoundary>
+            <Details params={params} />;
+        </ErrorBoundary >
+    );
 };
 //can't use hooks in class comps
 
